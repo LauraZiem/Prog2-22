7720aeec21d2523ccedd221b8fc600123324c81a
Laura Ziem Teles França <375278@unoesc.lan>
2022-08-03 21:57:09 -0300

    Primeiro commit do trabalho da aula

---
+++ SisVet.Repositorie/VeterinarioRepositorie.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using SisVet.Model;
+
+namespace SisVet.Repositorie
+{
+    public class VeterinarioRepositorie
+    {
+        public bool Salvar(Veterinario veterinario)
+        {
+            return true;
+        }
+
+        public Veterinario Retornar(int veterinarioId)
+        {
+            Veterinario veterinario = new Veterinario(veterinarioId);
+
+            // Buscar dad0os do bd
+
+            if(veterinarioId == 1)
+            {
+                veterinario.Nome = "Laura";
+                veterinario.Sobrenome = "Ziem";
+                veterinario.Srv = "123456";
+                veterinario.Email = "laura.ziem12@gmail.com";
+                veterinario.Telefone = "49 9 9999 9999";
+            }
+
+            return veterinario;
+        }
+
+        public List<Veterinario> Retornar()
+        {
+            return new List<Veterinario>();
+        }
+    }
+}
